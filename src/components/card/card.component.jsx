abab433a5ac88b78f154a9aa3965038c6ba46585
tantatinta <bluekasia@gmail.com>
2020-04-20 23:08:57 -0400

    mapping cards working

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./card.style.css"
+
+export const Card = (props) => {
+  return (
+    <div className="card-container">
+      <img alt="robot" src={`https://robohash.org/${props.monster.id}?set=set3&size=180x180`}/>
+      <h3>{props.monster.name}</h3>
+      <p>{props.monster.email}</p>
+    </div>
+
+  )
+
+}
